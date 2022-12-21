9bd8fac5ca98b9afb849e8ef233cc37e720da130
Jeff Sontag <jeffsontag@Talas-MBP-50.localdomain>
2022-12-21 15:00:28 -0800

    remove fix

--- helpers/render.py
+++ helpers/render.py
@@ -178,18 +178,12 @@ def render_animation(args, anim_args, animation_prompts, root):
             turbo_next_image, turbo_next_frame_idx = sample_to_cv2(prev_sample, type=np.float32), last_frame
             turbo_prev_image, turbo_prev_frame_idx = turbo_next_image, turbo_next_frame_idx
             start_frame = last_frame+turbo_steps
-        if args.use_mask and args.overlay_mask:
-            args.init_sample_raw = prev_sample
-            print(path)
-        if anim_args.use_mask_video:
-            mask_frame = os.path.join(args.outdir, 'maskframes', f"{last_frame+1:05}.jpg")
-            args.mask_file = mask_frame
-            print(mask_frame)
 
     args.n_samples = 1
     frame_idx = start_frame
     while frame_idx < anim_args.max_frames:
         print(f"Rendering animation frame {frame_idx} of {anim_args.max_frames}")
+        print(prev_sample)
         noise = keys.noise_schedule_series[frame_idx]
         strength = keys.strength_schedule_series[frame_idx]
         contrast = keys.contrast_schedule_series[frame_idx]
